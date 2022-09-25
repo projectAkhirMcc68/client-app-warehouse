1eaebc66001b764a453fe5ca309cac888a827b7b
adityajalu <101347792+adityajalu@users.noreply.github.com>
2022-09-25 23:31:20 +0700

    membuat project client app

---
+++ src/main/java/mii/co/id/clientappwarehouse/ClientAppWarehouseApplication.java
@@ -0,0 +1,13 @@
+package mii.co.id.clientappwarehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientAppWarehouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientAppWarehouseApplication.class, args);
+	}
+
+}
