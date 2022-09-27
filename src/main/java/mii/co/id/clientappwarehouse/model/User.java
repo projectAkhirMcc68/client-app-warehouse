b1e8286f0957f948c3b29bf23f3703a445e0a325
Teguh <teguhtsumarno@gmail.com>
2022-09-27 14:38:53 +0700

    manambhakan style.css,main.js dan index/dashboard.html

---
+++ src/main/java/mii/co/id/clientappwarehouse/model/User.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mii.co.id.clientappwarehouse.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ *
+ * @author USER
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    
+    private Long id;
+    private String username;
+    private String password;
+    
+}
